a3befee0de25660acb25dcc6b89682f4a11c778b
oscarfernandovalencia <osvamail02@gmail.com>
2022-09-27 17:39:13 -0500

    quite unos links del script

--- Materias/prueba.js
+++ Materias/prueba.js
@@ -1,7 +1,7 @@
 // script para abrir varios vínculos a la vez
 
 function abrirVinculo(){
-    const misLinks=["https://youtu.be/WQFrdaWPqcA/","https://youtu.be/WQFrdaWPqcA/","https://lqtecno.blogspot.com/2013/11/plataforma-elevadora-camion-julio-2013.html","http://www.tecfor.cl/productos/plataformas.php","https://gestortruck.com/plataformas-de-carga-para-camion/","https://www.tauro.mx/rampas-hidraulicas-railift-maxon/","http://www.solucionesenaccesosmg.com.mx/producto/rampa-oculta-de-uso-rudo","https://spanish.alibaba.com/product-detail/hydraulic-tail-lifts-for-trucks-and-forklift-60627502476.html"];
+    const misLinks=["https://lqtecno.blogspot.com/2013/11/plataforma-elevadora-camion-julio-2013.html","https://gestortruck.com/plataformas-de-carga-para-camion/"];
     misLinks.forEach(function(valor){
     window.open(valor);
     console.log(valor);
