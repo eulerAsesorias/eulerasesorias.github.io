86ad3ece94afcfe48170598725c967f1517fcbbb
oscarfernandovalencia <osvamail02@gmail.com>
2022-09-26 20:16:13 -0500

    optimizando codigo

--- Materias/prueba.js
+++ Materias/prueba.js
@@ -1,24 +1,9 @@
-// function abrirVinculo(){
-//     window.open("https://youtu.be/WQFrdaWPqcA/","Nueva","popup");
-//     window.open("https://gestortruck.com/plataformas-de-carga-para-camion/","nueva","popup");
-//     window.open("https://lqtecno.blogspot.com/2013/11/plataforma-elevadora-camion-julio-2013.html","nueva","popup");
-//     window.open("http://www.tecfor.cl/productos/plataformas.php","nueva","popup");
-//     window.open("https://gestortruck.com/plataformas-de-carga-para-camion/","nueva","popup");
-//     window.open("https://www.tauro.mx/rampas-hidraulicas-railift-maxon/","nueva","popup")
-//     window.open("http://www.solucionesenaccesosmg.com.mx/producto/rampa-oculta-de-uso-rudo","nueva","popup");
-//     window.open("https://spanish.alibaba.com/product-detail/hydraulic-tail-lifts-for-trucks-and-forklift-60627502476.html","nueva","popup");
-// }
-
+// script para abrir varios vínculos a la vez
 
 function abrirVinculo(){
-    window.open("https://youtu.be/WQFrdaWPqcA/","_blank");
-    window.open("https://gestortruck.com/plataformas-de-carga-para-camion/","_blank");
-    window.open("https://youtu.be/WQFrdaWPqcA/","_blank");
-    window.open("https://lqtecno.blogspot.com/2013/11/plataforma-elevadora-camion-julio-2013.html","_blank");
-    window.open("http://www.tecfor.cl/productos/plataformas.php","_blank");
-    window.open("https://gestortruck.com/plataformas-de-carga-para-camion/","_blank");
-    window.open("https://www.tauro.mx/rampas-hidraulicas-railift-maxon/","_blank");
-    window.open("http://www.solucionesenaccesosmg.com.mx/producto/rampa-oculta-de-uso-rudo","_blank");
-    window.open("https://spanish.alibaba.com/product-detail/hydraulic-tail-lifts-for-trucks-and-forklift-60627502476.html","_blank");
-}
-
+    const misLinks=["https://youtu.be/WQFrdaWPqcA/","https://youtu.be/WQFrdaWPqcA/","https://lqtecno.blogspot.com/2013/11/plataforma-elevadora-camion-julio-2013.html","http://www.tecfor.cl/productos/plataformas.php","https://gestortruck.com/plataformas-de-carga-para-camion/","https://www.tauro.mx/rampas-hidraulicas-railift-maxon/","http://www.solucionesenaccesosmg.com.mx/producto/rampa-oculta-de-uso-rudo","https://spanish.alibaba.com/product-detail/hydraulic-tail-lifts-for-trucks-and-forklift-60627502476.html"];
+    misLinks.forEach(function(valor){
+    window.open(valor);
+    console.log(valor);
+    }
+)}
