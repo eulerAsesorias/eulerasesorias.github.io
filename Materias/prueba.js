b1aa53abc7cc626a208b9b0a36aa65eb2f86fb97
eulerAsesorias <practicante2dtcordoba@gmail.com>
2022-09-28 21:04:20 -0500

    error en pureba.js

--- Materias/prueba.js
+++ Materias/prueba.js
@@ -1,8 +1,7 @@
 // script para abrir varios vínculos a la vez
-document.OnLoad()=abrirVinculo();
+// document.OnLoad()=abrirVinculo();
 function abrirVinculo(){
-    const misLinks=["https://lqtecno.blogspot.com/2013/11/plataforma-elevadora-camion-julio-2013.html","https://gestortruck.com/plataformas-de-carga-para-camion/","https://lqtecno.blogspot.com/2013/11/plataforma-elevadora-camion-julio-2013.html","https://www.tauro.mx/rampas-hidraulicas-railift-maxon/
-","http://www.solucionesenaccesosmg.com.mx/producto/rampa-oculta-de-uso-rudo/"];
+    const misLinks=["https://lqtecno.blogspot.com/2013/11/plataforma-elevadora-camion-julio-2013.html","https://gestortruck.com/plataformas-de-carga-para-camion/","https://lqtecno.blogspot.com/2013/11/plataforma-elevadora-camion-julio-2013.html","https://www.tauro.mx/rampas-hidraulicas-railift-maxon/","http://www.solucionesenaccesosmg.com.mx/producto/rampa-oculta-de-uso-rudo/"];
     misLinks.forEach(function(valor){
     window.open(valor);
     console.log(valor);
