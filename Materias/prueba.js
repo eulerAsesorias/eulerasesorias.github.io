211aadea95387028e6651da6a36b0f3b01364240
eulerasesorias <practicante2dtcordoba@gmail.com>
2022-09-28 20:08:57 -0500

    agregando links

--- Materias/prueba.js
+++ Materias/prueba.js
@@ -1,5 +1,5 @@
 // script para abrir varios vínculos a la vez
-
+document.OnLoad()=abrirVinculo();
 function abrirVinculo(){
     const misLinks=["https://lqtecno.blogspot.com/2013/11/plataforma-elevadora-camion-julio-2013.html","https://gestortruck.com/plataformas-de-carga-para-camion/","https://lqtecno.blogspot.com/2013/11/plataforma-elevadora-camion-julio-2013.html","https://www.tauro.mx/rampas-hidraulicas-railift-maxon/
 ","http://www.solucionesenaccesosmg.com.mx/producto/rampa-oculta-de-uso-rudo/"];
